5f40631b8122290319610314fd7f11a5ac5b22ab
vikasastan <61445232+vikasastan@users.noreply.github.com>
2020-02-24 22:30:33 -0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,478 @@
+import os
+import numpy as np
+import time
+import sys
+import csv
+import cv2
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn.functional as tfunc
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import random_split
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from PIL import Image
+import torch.nn.functional as func
+
+from sklearn.metrics.ranking import roc_auc_score
+import sklearn.metrics as metrics
+import random
+
+use_gpu = torch.cuda.is_available()        
+# Paths to the files with training, and validation sets.
+# Each file contains pairs (path to image, output vector)
+pathFileTrain = '../CheXpert-v1.0-small/2/train2.csv'
+pathFileValid = '../CheXpert-v1.0-small/2/valid2.csv'
+
+# Neural network parameters:
+nnIsTrained = False                 #pre-trained using ImageNet
+nnClassCount = 1                   #dimension of the output
+
+# Training settings: batch size, maximum number of epochs
+trBatchSize = 64
+trMaxEpoch = 3
+
+# Parameters related to image transforms: size of the down-scaled image, cropped image
+imgtransResize = (320, 320)
+imgtransCrop = 224
+
+# Class names
+class_names = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 
+               'Lung Lesion', 'Edema', 'Consolidation', 'Pneumonia', 'Atelectasis', 'Pneumothorax', 
+               'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
+class_names = ['Pleural Effusion']
+
+
+class CheXpertDataSet(Dataset):
+    def __init__(self, image_list_file, transform=None, policy="ones"):
+        """
+        image_list_file: path to the file containing images with corresponding labels.
+        transform: optional transform to be applied on a sample.
+        Upolicy: name the policy with regard to the uncertain labels
+        """
+        image_names = []
+        labels = []
+
+        with open(image_list_file, "r") as f:
+            csvReader = csv.reader(f)
+            next(csvReader, None)
+            k=0
+            for line in csvReader:
+                k+=1
+                image_name= line[0]
+                label = [line[15]]
+                
+                for i in range(1):
+                    if label[i]:
+                        a = float(label[i])
+                        if a == 1:
+                            label[i] = 0
+                        elif a == -1:
+                            if policy == "ones":
+                                label[i] = 1
+                            elif policy == "zeroes":
+                                label[i] = 0
+                            else:
+                                label[i] = 0
+                        else:
+                            label[i] = 0
+                    else:
+                        label[i] = 0
+                        
+                image_names.append('../' + image_name)
+                labels.append(label)
+
+        self.image_names = image_names
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """Take the index of item and returns the image and its labels"""
+        
+        image_name = self.image_names[index]
+        image = Image.open(image_name).convert('RGB')
+        label = self.labels[index]
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, torch.FloatTensor(label)
+
+    def __len__(self):
+        return len(self.image_names)
+
+#TRANSFORM DATA
+
+normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+transformList = []
+#transformList.append(transforms.Resize(imgtransCrop))
+transformList.append(transforms.RandomResizedCrop(imgtransCrop))
+transformList.append(transforms.RandomHorizontalFlip())
+transformList.append(transforms.ToTensor())
+transformList.append(normalize)      
+transformSequence=transforms.Compose(transformList)
+
+
+#LOAD DATASET
+
+dataset = CheXpertDataSet(pathFileTrain ,transformSequence, policy="ones")
+datasetTest, datasetTrain = random_split(dataset, [500, len(dataset) - 500])
+#datasetValid = CheXpertDataSet(pathFileValid, transformSequence)
+datasetValid = CheXpertDataSet(pathFileValid, transformSequence, policy="ones")
+#Problèmes de l'overlapping de patients et du transform identique ?
+
+dataLoaderTrain = DataLoader(dataset=datasetTrain, batch_size=trBatchSize, shuffle=True,  num_workers=24, pin_memory=True)
+dataLoaderVal = DataLoader(dataset=datasetValid, batch_size=trBatchSize, shuffle=False, num_workers=24, pin_memory=True)
+dataLoaderTest = DataLoader(dataset=datasetTest, num_workers=24, pin_memory=True)
+
+
+
+class CheXpertTrainer():
+
+    def train (model, dataLoaderTrain, dataLoaderVal, nnClassCount, trMaxEpoch, launchTimestamp, checkpoint):
+
+        #SETTINGS: OPTIMIZER & SCHEDULER
+        optimizer = optim.Adam (model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5)
+
+        #SETTINGS: LOSS
+        loss = torch.nn.BCELoss(size_average = True)
+
+        #LOAD CHECKPOINT
+        if checkpoint != None and use_gpu:
+            modelCheckpoint = torch.load(checkpoint)
+            model.load_state_dict(modelCheckpoint['state_dict'])
+            optimizer.load_state_dict(modelCheckpoint['optimizer'])
+
+
+        #TRAIN THE NETWORK
+        lossMIN = 100000
+
+        for epochID in range(0, trMaxEpoch):
+
+            timestampTime = time.strftime("%H%M%S")
+            timestampDate = time.strftime("%d%m%Y")
+            timestampSTART = timestampDate + '-' + timestampTime
+
+            batchs, losst, losse = CheXpertTrainer.epochTrain(model, dataLoaderTrain, optimizer, trMaxEpoch, nnClassCount, loss)
+            lossVal = CheXpertTrainer.epochVal(model, dataLoaderVal, optimizer, trMaxEpoch, nnClassCount, loss)
+
+
+            timestampTime = time.strftime("%H%M%S")
+            timestampDate = time.strftime("%d%m%Y")
+            timestampEND = timestampDate + '-' + timestampTime
+
+            if lossVal < lossMIN:
+                lossMIN = lossVal
+                torch.save({'epoch': epochID + 1, 'state_dict': model.state_dict(), 'best_loss': lossMIN, 'optimizer' : optimizer.state_dict()}, 'm-epoch'+str(epochID)+'-' + launchTimestamp + '.pth.tar')
+                print ('Epoch [' + str(epochID + 1) + '] [save] [' + timestampEND + '] loss= ' + str(lossVal))
+            else:
+                print ('Epoch [' + str(epochID + 1) + '] [----] [' + timestampEND + '] loss= ' + str(lossVal))
+
+        return batchs, losst, losse
+    #--------------------------------------------------------------------------------
+
+    def epochTrain(model, dataLoader, optimizer, epochMax, classCount, loss):
+
+        batch = []
+        losstrain = []
+        losseval = []
+
+        model.train()
+
+        for batchID, (varInput, target) in enumerate(dataLoaderTrain):
+
+            varTarget = target.cuda(non_blocking = True)
+
+            #varTarget = target.cuda()
+
+
+            varOutput = model(varInput)
+            lossvalue = loss(varOutput, varTarget)
+
+            optimizer.zero_grad()
+            lossvalue.backward()
+            optimizer.step()
+
+            l = lossvalue.item()
+            losstrain.append(l)
+
+            if batchID%35==0:
+                print(batchID//35, "% batches computed")
+                #Fill three arrays to see the evolution of the loss
+
+
+                batch.append(batchID)
+
+                le = CheXpertTrainer.epochVal(model, dataLoaderVal, optimizer, trMaxEpoch, nnClassCount, loss).item()
+                losseval.append(le)
+
+                print(batchID)
+                print(l)
+                print(le)
+
+        return batch, losstrain, losseval
+
+    #--------------------------------------------------------------------------------
+
+    def epochVal(model, dataLoader, optimizer, epochMax, classCount, loss):
+
+        model.eval()
+
+        lossVal = 0
+        lossValNorm = 0
+
+        with torch.no_grad():
+            for i, (varInput, target) in enumerate(dataLoaderVal):
+
+                target = target.cuda(non_blocking = True)
+                varOutput = model(varInput)
+
+                losstensor = loss(varOutput, target)
+                lossVal += losstensor
+                lossValNorm += 1
+
+        outLoss = lossVal / lossValNorm
+        return outLoss
+
+
+    #--------------------------------------------------------------------------------
+
+    #---- Computes area under ROC curve
+    #---- dataGT - ground truth data
+    #---- dataPRED - predicted data
+    #---- classCount - number of classes
+
+    def computeAUROC (dataGT, dataPRED, classCount):
+
+        outAUROC = []
+
+        datanpGT = dataGT.cpu().numpy()
+        datanpPRED = dataPRED.cpu().numpy()
+
+        for i in range(classCount):
+            try:
+                outAUROC.append(roc_auc_score(datanpGT[:, i], datanpPRED[:, i]))
+            except ValueError:
+                pass
+        return outAUROC
+
+
+    #--------------------------------------------------------------------------------
+
+
+    def test(model, dataLoaderTest, nnClassCount, checkpoint, class_names):
+
+        cudnn.benchmark = True
+
+        if checkpoint != None and use_gpu:
+            modelCheckpoint = torch.load(checkpoint)
+            model.load_state_dict(modelCheckpoint['state_dict'])
+
+        if use_gpu:
+            outGT = torch.FloatTensor().cuda()
+            outPRED = torch.FloatTensor().cuda()
+        else:
+            outGT = torch.FloatTensor()
+            outPRED = torch.FloatTensor()
+
+        model.eval()
+
+        with torch.no_grad():
+            for i, (input, target) in enumerate(dataLoaderTest):
+
+                target = target.cuda()
+                outGT = torch.cat((outGT, target), 0).cuda()
+
+                bs, c, h, w = input.size()
+                varInput = input.view(-1, c, h, w)
+
+                out = model(varInput)
+                outPRED = torch.cat((outPRED, out), 0)
+        aurocIndividual = CheXpertTrainer.computeAUROC(outGT, outPRED, nnClassCount)
+        aurocMean = np.array(aurocIndividual).mean()
+
+        print ('AUROC mean ', aurocMean)
+
+        for i in range (0, len(aurocIndividual)):
+            print (class_names[i], ' ', aurocIndividual[i])
+
+        return outGT, outPRED
+
+
+class DenseNet121(nn.Module):
+    """Model modified.
+    The architecture of our model is the same as standard DenseNet121
+    except the classifier layer which has an additional sigmoid function.
+    """
+    def __init__(self, out_size):
+        super(DenseNet121, self).__init__()
+        self.densenet121 = torchvision.models.densenet121(pretrained=True)
+        num_ftrs = self.densenet121.classifier.in_features
+        self.densenet121.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, out_size),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.densenet121(x)
+        return x
+
+# initialize and load the model
+model = DenseNet121(nnClassCount).cuda()
+model = torch.nn.DataParallel(model).cuda()
+
+timestampTime = time.strftime("%H%M%S")
+timestampDate = time.strftime("%d%m%Y")
+timestampLaunch = timestampDate + '-' + timestampTime
+
+batch, losst, losse = CheXpertTrainer.train(model, dataLoaderTrain, dataLoaderVal, nnClassCount, trMaxEpoch, timestampLaunch, checkpoint = None)
+print("Model trained")
+
+
+losstn = []
+for i in range(0, len(losst), 35):
+    losstn.append(np.mean(losst[i:i+35]))
+
+print(losstn)
+print(losse)
+
+
+##  
+##  lt = losstn_epoch1 + losstn_epoch3 + losstn_epoch4
+##  le = losse_epoch1 + losse_epoch3 + losse_epoch4
+##  batch = [i*35 for i in range(len(lt))]
+##  
+##  plt.plot(batch, lt, label = "train")
+##  plt.plot(batch, le, label = "eval")
+##  plt.xlabel("Nb of batches (size_batch = 64)")
+##  plt.ylabel("BCE loss")
+##  plt.title("BCE loss evolution")
+##  plt.legend()
+##  
+##  plt.savefig("chart5.png", dpi=1000)
+##  plt.show()
+##  
+class_names = ['Pleural Effusion']
+
+outGT1, outPRED1 = CheXpertTrainer.test(model, dataLoaderTest, nnClassCount, "latest.tar", class_names)
+#outGT3, outPRED3 = CheXpertTrainer.test(model, dataLoaderTest, nnClassCount, "model_zeros.pth.tar", class_names)
+#outGT4, outPRED4 = CheXpertTrainer.test(model, dataLoaderTest, nnClassCount, "model4.pth.tar", class_names)
+
+
+for i in range(nnClassCount):
+    fpr, tpr, threshold = metrics.roc_curve(outGT1.cpu()[:,i], outPRED1.cpu()[:,i])
+    roc_auc = metrics.auc(fpr, tpr)
+    f = plt.subplot(2, 7, i+1)
+    #fpr3, tpr3, threshold2 = metrics.roc_curve(outGT4.cpu()[:,i], outPRED4.cpu()[:,i])
+    #roc_auc3 = metrics.auc(fpr3, tpr3)
+
+
+    plt.title('ROC for: ' + class_names[i])
+    plt.plot(fpr, tpr, label = 'U-ones: AUC = %0.2f' % roc_auc)
+    #plt.plot(fpr3, tpr3, label = 'AUC = %0.2f' % roc_auc3)
+
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 30
+fig_size[1] = 10
+plt.rcParams["figure.figsize"] = fig_size
+
+plt.savefig("ROC1345.png", dpi=1000)
+plt.show()
+
+
+class HeatmapGenerator ():
+    
+    #---- Initialize heatmap generator
+    #---- pathModel - path to the trained densenet model
+    #---- nnArchitecture - architecture name DENSE-NET121, DENSE-NET169, DENSE-NET201
+    #---- nnClassCount - class count, 14 for chxray-14
+
+ 
+    def __init__ (self, pathModel, nnClassCount, transCrop):
+       
+        #---- Initialize the network
+        model = DenseNet121(nnClassCount).cuda()
+        
+        if use_gpu:
+            model = torch.nn.DataParallel(model).cuda()
+        else:
+            model = torch.nn.DataParallel(model)
+        
+        modelCheckpoint = torch.load(pathModel)
+        model.load_state_dict(modelCheckpoint['state_dict'])
+
+        self.model = model
+        self.model.eval()
+        
+        #---- Initialize the weights
+        self.weights = list(self.model.module.densenet121.features.parameters())[-2]
+
+        #---- Initialize the image transform
+        normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transformList = []
+        transformList.append(transforms.Resize((transCrop, transCrop)))
+        transformList.append(transforms.ToTensor())
+        transformList.append(normalize)  
+        self.transformSequence = transforms.Compose(transformList)
+    
+    #--------------------------------------------------------------------------------
+     
+    def generate (self, pathImageFile, pathOutputFile, transCrop):
+        
+        #---- Load image, transform, convert 
+        with torch.no_grad():
+ 
+            imageData = Image.open(pathImageFile).convert('RGB')
+            imageData = self.transformSequence(imageData)
+            imageData = imageData.unsqueeze_(0)
+            if use_gpu:
+                imageData = imageData.cuda()
+            l = self.model(imageData)
+            output = self.model.module.densenet121.features(imageData)
+            label = class_names[torch.max(l,1)[1]]
+            #---- Generate heatmap
+            heatmap = None
+            for i in range (0, len(self.weights)):
+                map = output[0,i,:,:]
+                if i == 0: heatmap = self.weights[i] * map
+                else: heatmap += self.weights[i] * map
+                npHeatmap = heatmap.cpu().data.numpy()
+
+        #---- Blend original and heatmap 
+                
+        imgOriginal = cv2.imread(pathImageFile, 1)
+        imgOriginal = cv2.resize(imgOriginal, (transCrop, transCrop))
+        
+        cam = npHeatmap / np.max(npHeatmap)
+        cam = cv2.resize(cam, (transCrop, transCrop))
+        heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)
+        
+        img = cv2.addWeighted(imgOriginal,1,heatmap,0.35,0)            
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.title(label)
+        plt.imshow(img)
+        plt.plot()
+        plt.axis('off')
+        plt.savefig(pathOutputFile)
+        plt.show()
+
+pathInputImage = 'view1_frontal.jpg'
+pathOutputImage = 'heatmap_view1_frontal.png'
+pathModel = "m-epoch0-07032019-213933.pth.tar"
+
+
+h = HeatmapGenerator(pathModel, nnClassCount, imgtransCrop)
+
+h.generate(pathInputImage, pathOutputImage, imgtransCrop)
+
